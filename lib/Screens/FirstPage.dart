eb25f0264ee364b088022bc7f3bb83cfa12fa55b
Nihalbacker <nihalbackerak@gmail.com>
2023-11-04 13:36:55 +0530

    Initial commit

---
+++ lib/Screens/FirstPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.tealAccent,
+        appBar: AppBar(title: Text('Profile App'),
+        ),
+        body: Column(
+
+    )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+import 'package:profile2/Screens/FirstPage.dart';
+
+void main(){
+  runApp(MaterialApp(home:FirstPage()));
+}
